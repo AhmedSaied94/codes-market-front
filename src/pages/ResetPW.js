c47f698490757e543c832fa42cea021fef62f74f
Ahmed Saied <ahmed.saeed311294@gmail.com>
2022-03-17 22:18:40 +0200

    build v1

--- src/pages/ResetPW.js
+++ src/pages/ResetPW.js
@@ -8,7 +8,7 @@ const ResetPW = () => {
     const handleFinish = values => {
         axiosInstance.post('/account/password-reset-request/', {
             "email":values.email,
-            "redirect_url":"https://foxsourcecode.herokuapp.com/password-reset-confirm"
+            "redirect_url":"http://52.24.242.40/password-reset-confirm"
         })
         .then(res => {
             message.success('an email sent check your mail inbox you will be redirect to home')
