4c9e205c16b96b44ab521589032daa03183709a6
Ahmed Saied <ahmed.saeed311294@gmail.com>
2022-03-18 03:04:44 +0200

    change ip

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { message } from 'antd';
 
-const baseUrl = 'http://52.24.242.40/api'
+const baseUrl = 'http://http://44.229.203.159/api'
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl,
